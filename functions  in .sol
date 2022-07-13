93e1796518074d974dbcd108f9469e90b3a0b55c
Aniket Jadhao <101780497+JadhaoAniket@users.noreply.github.com>
2022-07-13 17:46:54 +0530

    Create functions  in .sol

---
+++ functions  in .sol
@@ -0,0 +1,20 @@
+contract learnFunction{
+
+    // function-name(parameter-list) scope returns() {
+        //statements }
+    
+    function remoteControlOpen(bool closedDoor) public  returns(bool)
+    {
+   
+    // all the code of the function - the script - the statement 
+  
+    }
+// create a function that can add up two variables 
+
+function addValues()public view  returns (uint) {
+    uint a = 2;
+    uint b = 3;
+    uint result = a + b;
+    return result;
+   }
+}
